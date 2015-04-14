0a0fd61f3eb2928be3155ecdbe7d77292f61c140
Artur Zayats <zag2art@gmail.com>
2015-04-14 17:05:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var mongo = require('mongodb');
+var async = require('async');
+
+var url = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+
+mongo.MongoClient.connect(url, function(err, db){
+  if(err) throw err;
+
+  var date = weekAgo();
+
+  db.collections(function(err, collections) {
+
+    async.eachSeries(collections, function(col, cb){
+      var name = col.collectionName;
+
+      if (name.indexOf('_ops') === -1 || name === 'auths_ops') return cb();
+
+      col.remove({'m.ts': {$lt: date}}, function(err, res){
+        console.log('collection', col.collectionName, res.result);
+        cb()
+      });
+
+    }, function(){
+      db.close();
+    });
+  });
+
+});
+
+function weekAgo(){
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+  return +oneWeekAgo;
+}
