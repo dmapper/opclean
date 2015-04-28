581fd6643777cf335da0c81b00cd32965d3d071d
Artur Zayats <zag2art@gmail.com>
2015-04-28 15:10:28 +0300

    add preserving last version of docs

--- index.js
+++ index.js
@@ -3,24 +3,62 @@ var async = require('async');
 
 var url = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
+var excludes = ['auths_ops'];
+
+var cleanDate = daysAgoFromNow(7);
+
+function cleanCollection(db, oplogsCollectionName, done){
+  var snapshotsCollectionName = oplogsCollectionName.split('_ops')[0];
+
+  if (!snapshotsCollectionName) {
+    throw new Error('Cant get collection name from ops-collection: ' + oplogsCollectionName);
+  }
+
+  var snapshotsCollection = db.collection(snapshotsCollectionName);
+  var oplogsCollection = db.collection(oplogsCollectionName);
+
+  snapshotsCollection.find().toArray(function (err, snapshots) {
+    if (err) throw err;
+
+    snapshots = snapshots || [];
+    var counter = 0;
+
+    async.eachSeries(snapshots, function(snapshot, cb){
+      var query = {
+        'name': snapshot._id,
+        'v': { $ne: snapshot._v - 1 },
+        'm.ts': { $lt: cleanDate }
+      };
+
+      oplogsCollection.remove(query, function(err, res){
+
+        counter += res.result.n;
+        cb()
+      });
+    }, function(){
+      console.log('collection', oplogsCollectionName, counter);
+      done();
+    });
+
+  });
+}
+
 mongo.MongoClient.connect(url, function(err, db){
   if(err) throw err;
 
-  var date = weekAgo();
-
   db.collections(function(err, collections) {
 
-    async.eachSeries(collections, function(col, cb){
-      var name = col.collectionName;
+    var cols = [];
+    collections.forEach(function(col){
+      var name = name = col.collectionName;
+      if (name.indexOf('_ops') !== -1 && excludes.indexOf(name) === -1){
+        cols.push(name);
+      }
+    });
 
-      if (name.indexOf('_ops') === -1 || name === 'auths_ops') return cb();
+    console.log('Collections: ', cols.join(', '));
 
-      col.remove({'m.ts': {$lt: date}}, function(err, res){
-        console.log('collection', col.collectionName, res.result);
-        cb()
-      });
-
-    }, function(){
+    async.eachSeries(cols, cleanCollection.bind(null, db), function(){
       db.close();
       process.exit()
     });
@@ -28,9 +66,8 @@ mongo.MongoClient.connect(url, function(err, db){
 
 });
 
-function weekAgo(){
-  var oneWeekAgo = new Date();
-  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-  return +oneWeekAgo;
+function daysAgoFromNow(days){
+  var date = new Date();
+  date.setDate(date.getDate() - days);
+  return +date;
 }
